eb0323113e80f870a79a5c25ef4250b8729e6004
DOSSANTOSDaniel <danielitto91@gmail.com>
2019-04-24 23:57:31 +0200

    update

---
+++ ConfigBackupSRV.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#	ConfigBackupSRV
+#
+# Description:
+#	Script permettant:
+#	1- 
+#----------------------------------------------------------------#
+# Usage: ./
+#	Exécuter le script en root!
+#
+# Auteur:
+#  	Daniel DOS SANTOS < daniel.massy91@gmail.com >
+#----------------------------------------------------------------#
