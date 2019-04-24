54a57d44a13bc93c064ee3bbc52965015504b24b
DOSSANTOSDaniel <danielitto91@gmail.com>
2019-04-24 23:07:13 +0200

    Vérsion utilisable ok

--- InstallConfigClientWin.ps1
+++ InstallConfigClientWin.ps1
@@ -2,13 +2,12 @@
 NOM DU SCRIPT: InstallConfigClientWin.ps1			
 *================================================================*
 DESCRIPTION:
-
- Le script va nous permettre de ::
+ Le script va nous permettre de :
 
  1 :Installer le client Windows de BackupPC
  2 :Configurer la règle de firewall pour Rsync(873)
- 3 :Enregistre dans un fichier le nom netbios du poste
- 4 :Enregistre des informations sur le réseau		
+ 3 :Enregistre dans un fichier le nom netbios du poste et 
+ d'autres informations.
 *================================================================*
 AUTEUR: Daniel DOS SANTOS < daniel.massy91@gmail.com >			
 *================================================================*
@@ -16,71 +15,64 @@ DATE DE CREATION: 24/04/2019
 *================================================================*  
 USAGE: PS C:\Users> powershell ./InstallConfigClientWin.ps1		
 *================================================================*
-NOTES: 
-    
-*================================================================*
 POWERSHELL VERSION: 5.1  
 *================================================================*
-    RESTE A FAIRE
-    ----------------
-
-    1* 
 #>
 
-####   fonctions    ###
-############################################
-function message 
+### Fonctions ###
+function message ($1)
 {
   Write-Host " " 
   Write-Host "$1"
-  Write-Host "---------------------------" -f green -b black
+  Write-Host "---------------------------------------------------------------------------------" -f green -b black
   Write-Host " "
 }
 
-##### Début du script #####
-## message d'accueil  ##
+### Début du code ###
+
+# Message d'accueil
 write-host ""
 write-host "-----------------------------------------"
 write-host "*                                       *"
-write-host "*             BIENVENUE                 *"
-write-host "*                                       *"
-write-host "* SCRIPT << BackupPC client install >> *"
+write-host "*      Script client BackupPC           *"
 write-host "*                                       *"
 write-host "-----------------------------------------" 
 write-host ""
 Start-Sleep 2
 
-# Activation de l'exécution de scripts sur l'ordinateur
-message "Activation de l'éxécution de scripts"
-$auth = get-executionpolicy
+# Dossier principale
+mkdir $env:USERPROFILE\atout
 
-#si = restricted, allsigned, remotesigned alors configurer l'autorisation
-#si = Unrestricted ne rien faire c'est ok
-if ($auth -eq "Unrestricted") {
-  Write-Host "Authorisation acceptée"
-}
-else {
-  Write-Host "Modification de l'authorisation à Unrestricted"
-  set-executionpolicy -executionpolicy unrestricted -Force
-}
-
-message "Téléchargement du client Windows pour BackupPC"
-Invoke-WebRequest -Uri https://www.logicbox.fr/download/DesktopClient_LogicBackup_1.0.0.1_installer.exe -OutFile "C:\"
+message "Téléchargement du client Windows pour BackupPC en cours"
+Invoke-WebRequest -Uri https://www.logicbox.fr/download/DesktopClient_LogicBackup_1.0.0.1_installer.exe -OutFile $env:USERPROFILE\atout\DesktopClient_LogicBackup_1.0.0.1_installer.exe
+Clear-Host
 
 # "/S" pour une installation non graphique
-message "Installation du client"
-Start-Process 'C:\DesktopClient_LogicBackup_1.0.0.1_installer.exe' "/S"
+message "Installation du client en cours"
+Start-Process $env:USERPROFILE\atout\DesktopClient_LogicBackup_1.0.0.1_installer.exe "/S"
+Clear-Host
 
 # Configuration du firewall de Windows
-message "Ouverture dur port TCP 873 en trafic entrant"
+message "Ouverture dur port TCP 873 en trafic entrant en cours"
 new-netfirewallrule -name "LogicBackup" -displayname "LogicBackup port 873" -profile domain,public,private -enabled true -protocol TCP -localport 873 -Action allow
+Start-Sleep 6
+Clear-Host
 
 # Informations sur le poste client
-message "Récupération des informations utiles concernant le poste client"
-get-wmiobject Win32_computersystem >> poste_$env:userdomain.txt
+$chemin="$env:USERPROFILE\atout\poste_$env:userdomain.txt"
+message "Récupération des informations utiles concernant le poste client en cours"
+message "Informations poste client $env:userdomain" *>> $chemin
+get-wmiobject Win32_computersystem >> $chemin
+message "Nom Netbios" *>> $chemin
+$env:userdomain >> $chemin
+message "Configuration Réseau" *>> $chemin
+Get-NetIPConfiguration >> $chemin
+Start-Sleep 3
+Clear-Host
 
-message "Nom Netbios"
-$env:userdomain >> poste_$env:userdomain.txt
-
-message "Configuration Réseau"
-Get-NetIPConfiguration >> poste_$env:userdomain.txt
+message "Les informations ont été enregistrées dans le fichier : $chemin"
+write-host ""
+write-host "-----------------------------------------"
+write-host "*             Fin du script !           *"
+write-host "-----------------------------------------" 
+write-host ""
